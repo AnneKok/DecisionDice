c2f929fd2e6d3d03e8f257bbd04d417de97d953c
AnneKok <annelindakok@gmail.com>
2017-03-22 10:27:30 +0100

    Added gitignore... maybe

---
+++ app/src/main/java/com/example/decisiondice/PizzaActivity.java
@@ -0,0 +1,13 @@
+package com.example.decisiondice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PizzaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza);
+    }
+}
